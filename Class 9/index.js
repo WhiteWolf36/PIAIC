4c919d6a934de41298669e081cf28cdcdd442905
WhiteWolf36 <ahmadking78654@gmail.com>
2023-09-16 15:34:18 +0500

    creating a callback function

--- Class 9/index.js
+++ Class 9/index.js
@@ -14,15 +14,27 @@
 //2- Do some grossery
 //3- Laundry dress
 //4- Attend an event
-setTimeout(() => {
-    console.log("Car maintainainence");
-}, 2000);
-setTimeout(() => {
-    console.log("Do some grossey");
-}, 2000);
-setTimeout(() => {
-    console.log("TLaundry dress");
-}, 2000);
-setTimeout(() => {
-    console.log("Attend an event");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Car maintainainence");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("Do some grossey");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("TLaundry dress");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("Attend an event");
+// }, 2000);
+//=========================
+//Callbacks
+//what is a callback?
+//pass a function wihtin a function as a parameter
+function task1(callback) {
+    console.log("Do something");
+    callback("We are passing values to the function");
+}
+function callback(text) {
+    console.log("this is a callback function ", text);
+}
+task1(callback);
--- Class 9/index.ts
+++ Class 9/index.ts
@@ -16,18 +16,18 @@
 //3- Laundry dress
 //4- Attend an event
 
-setTimeout(() => {
-  console.log("Car maintainainence");
-}, 2000);
-setTimeout(() => {
-  console.log("Do some grossey");
-}, 2000);
-setTimeout(() => {
-  console.log("TLaundry dress");
-}, 2000);
-setTimeout(() => {
-  console.log("Attend an event");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Car maintainainence");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("Do some grossey");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("TLaundry dress");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("Attend an event");
+// }, 2000);
 
 //=========================
 //Callbacks
@@ -35,3 +35,14 @@ setTimeout(() => {
 //what is a callback?
 
 //pass a function wihtin a function as a parameter
+
+function task1(callback: (text: string) => void) {
+  console.log("Do something");
+  callback("We are passing values to the function");
+}
+
+function callback(text: string) {
+  console.log("this is a callback function ", text);
+}
+
+task1(callback);
