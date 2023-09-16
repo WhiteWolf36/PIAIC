f1081878d59c88a5d94ed3c7a8519cb9b7d67f99
WhiteWolf36 <ahmadking78654@gmail.com>
2023-09-16 15:43:56 +0500

    a problem =>undefined that why we use callbacks

--- Class 9/index.js
+++ Class 9/index.js
@@ -30,11 +30,33 @@
 //Callbacks
 //what is a callback?
 //pass a function wihtin a function as a parameter
-function task1(callback) {
-    console.log("Do something");
-    callback("We are passing values to the function");
+// function task1(callback: (text: string) => void) {
+//   console.log("Do something");
+//   callback("We are passing values to the function");
+// }
+// function callback(text: string) {
+//   console.log("this is a callback function ", text);
+// }
+// task1(callback);
+function carMaintainance() {
+    console.log("I left my car at 9 at workshop");
+    setTimeout(() => {
+        console.log("Car is fixed");
+        return "Your car is ready to pickup";
+    }, 2000);
 }
-function callback(text) {
-    console.log("this is a callback function ", text);
+function pickDress() {
+    setTimeout(() => {
+        return "Your dress is ready to pickup";
+    }, 1000);
 }
-task1(callback);
+let mechanicResponse = carMaintainance();
+console.log(mechanicResponse);
+setTimeout(() => {
+    console.log("Do some grossery");
+}, 2000);
+let laundryResponse = pickDress();
+console.log(laundryResponse);
+setTimeout(() => {
+    console.log("Attend event");
+}, 2000);
