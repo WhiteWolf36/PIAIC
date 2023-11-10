933cf8d0e7410e56c140ff726f02a17351712290
WhiteWolf36 <ahmadking78654@gmail.com>
2023-11-11 02:13:58 +0500

    Chapter 3 half

--- Quarter 2/tasks/src/app/page.tsx
+++ Quarter 2/tasks/src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
           <h1 className="text-6xl first-letter:text-yellow-600 font-bold text-blue-600 hover:text-gray-600 hover:cursor-pointer ">
             Modern Problems
           </h1>
-          <h2 className="text-3xl font-semibold text-blue-400">
+          <h2 className="text-3xl font-semibold text-blue-400 hover:pe-8 hover:cursor-help">
             Requires Modern Solution
           </h2>
-          <button className="my-4 px-4 py-2 bg-blue-500 rounded-lg text-white  border-2 border-black">
+          <button className="my-4 px-4 py-2 bg-blue-500 rounded-lg text-white  border-2 border-black ">
             Learn More
           </button>
         </div>
