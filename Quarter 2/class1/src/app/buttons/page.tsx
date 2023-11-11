57ec06dd3849e0369eb7095e0906b81c88921c5f
WhiteWolf36 <ahmadking78654@gmail.com>
2023-11-11 10:55:42 +0500

    Refactor button page component

--- Quarter 2/class1/src/app/buttons/page.tsx
+++ Quarter 2/class1/src/app/buttons/page.tsx
@@ -19,10 +19,10 @@ function Class2() {
           </div>
           <div>
             <p className="font-light text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis
-              sint ea aut nesciunt, modi repudiandae iusto voluptates sapiente
-              saepe dolor accusamus, officia cum? Aliquid blanditiis id neque
-              distinctio quam!
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Deleniti, minus pariatur ab itaque nisi tempore quo, doloribus
+              voluptatibus laudantium odit eligendi veniam blanditiis accusamus
+              earum? Recusandae laudantium architecto deserunt dolorem!
             </p>
           </div>
           <div className="flex justify-between text-sm ">
