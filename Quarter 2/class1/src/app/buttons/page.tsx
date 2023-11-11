7503ef6728b12bfa11c5ca74c9ec08efa418de28
WhiteWolf36 <ahmadking78654@gmail.com>
2023-11-11 10:18:10 +0500

    class 2 task 1

---
+++ Quarter 2/class1/src/app/buttons/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Class2() {
+  return (
+    <>
+      <div className="m-10 flex justify-between">
+        <button className="p-2 text-white rounded-md border-black bg-red-600">
+          Button 1
+        </button>
+        <button className="p-2 text-white rounded-md border-black bg-red-600">
+          Button 2
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Class2;
